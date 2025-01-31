d724832700239f68ea60fbbfc28ca47504ec0a71
Sean Boisen <sean@seanboisen.com>
2025-01-30 18:50:39 -0800

    added topic_vote_sum and self.topicvotesum
    added partial code for consolidate_intersections

--- src/openbibleinfo/reader.py
+++ src/openbibleinfo/reader.py
@@ -38,6 +38,8 @@ class Reader:
         # drop records with cross-chapter/book ranges
         self.topicsdf = self.df[self.df.UsableReference]
         self.toptopics = self.top_topics()
+        self.topicvotesum = self.topic_vote_sum()
+
 
     def display_topic_data(self, topic: str) -> None:
         """Display verses and votes for a topic."""
@@ -88,26 +90,27 @@ class Reader:
         else:
             return True
 
-    def passage_probability(self, row) -> float:
+    def topic_vote_sum(self):
+        """Return a DataFrame with summaries by topic."""
+        # this returns other columns: i'm not sure why
+        return self.topicsdf.groupby("Topic").sum("Votes")
+        
+    def passage_probability2(self, row) -> float:
         """Return the percentage of this row's votes for the whole topic.
 
-        Only computable given self.toptopics.
+        Only computable given self.toptopics. Attempting a faster approach.
         """
         if not row.UsableReference:
             return 0.0
         elif not self.toptopics.Topic.str.contains(row.Topic).any():
             return 0.0
         else:
-            rowtopic = self.toptopics[self.toptopics.Topic == row.Topic]
-            if rowtopic.empty:
+            votesum = int(self.topicvotesum.loc[row.Topic].Votes)
+            if not votesum:
                 return 0.0
             else:
-                votesum = int(rowtopic.TopicVotesSum.iloc[0])
-                if not votesum:
-                    return 0.0
-                else:
-                    votes = int(row.Votes)
-                    return votes / votesum
+                votes = int(row.Votes)
+                return votes / votesum
 
     def top_topics(self, threshold: int = 55) -> pd.DataFrame:
         """Return topics whose MeanPassageVotes exceed threshold."""
@@ -121,3 +124,36 @@ class Reader:
         topicsdf = pd.concat([passagecountdf, topicvotessum], axis="columns").drop("Topic2", axis="columns")
         topicsdf["MeanPassageVotes"] = topicsdf.TopicVotesSum / topicsdf.TopicPassageCount
         return topicsdf[topicsdf.MeanPassageVotes > threshold].sort_values("MeanPassageVotes", ascending=False)
+
+    def consolidate_intersections(self, weight: float = 0.75) -> pd.Dataframe:
+        """Return a new DataFrame that combines single verses with 2-verse ranges.
+
+        Single verse votes are weighted according to weight and added
+        to the votes for the range.
+
+        """
+
+        def intersects(rangetuple: pd.core.frame.Pandas, singletuple: pd.core.frame.Pandas) -> bool:
+            """Return True if singletuple falls within rangetuple.
+
+            Here 'within' simply means the single is the same as
+            either start or end. This should only be applied to tuples
+            with the same topic.
+
+            """
+            if rangetuple.UsableRange and (rangetuple.PassageLength == 2) and (singletuple.PassageLength == 1):
+                return ((rangetuple.StartVerseId == singletuple.StartVerseId) or
+                        (rangetuple.EndVerseId == singletuple.StartVerseId))
+            else:
+                return False
+
+        # within rows for each topic
+        #  ranges = (UsableRange == True)
+        #  singles = (UsableRange == False)
+        #  for each value in ranges:
+        #    for each value in singles:
+        #      if intersects(range, single):
+        #        add range to new df with weighted votes from single
+        #        remove single from singles so you don't process it again
+        #  finally add remaining ranges and singles
+        pass
